721516220a530efb2d18253c30252e98fea4fd39
decyma <soos3121@gmail.com>
2021-10-16 13:27:19 +0900

    feat: 훈련모드 추가

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import hydra
 
 from omegaconf import DictConfig
 
+from src.train.train_main import train_main
+
 
 @hydra.main(config_path=".", config_name="main_args")
 def main(cfg: DictConfig):
     print(cfg)
     print(cfg.model.config_name)
+    if cfg.mode == "train":
+        train_main(cfg)
 
 
 if __name__ == "__main__":
