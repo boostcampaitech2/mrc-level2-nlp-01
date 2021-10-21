96ec1b1aef83fe1c7a1e9a76b7b99d579a8964dc
decyma <soos3121@gmail.com>
2021-10-21 15:48:37 +0900

    feat : inference 구현

--- src/inference.py
+++ src/inference.py
@@ -1,12 +1,6 @@
-"""
-Open-Domain Question Answering 을 수행하는 inference 코드 입니다.
-
-대부분의 로직은 train.py 와 비슷하나 retrieval, predict 부분이 추가되어 있습니다.
-"""
-
-
 import logging
 import sys
+import os
 from typing import Callable, List, Dict, NoReturn, Tuple
 
 import numpy as np
@@ -35,61 +29,26 @@ from src.magic_box.utils_qa import postprocess_qa_predictions, check_no_error
 from src.magic_box.train_qa import QuestionAnsweringTrainer
 from src.retrieval import SparseRetrieval
 
-from arguments import (
-    ModelArguments,
-    DataTrainingArguments,
-)
 
-
-logger = logging.getLogger(__name__)
-
-
-def main():
+def inference(model_args, data_args):
     # 가능한 arguments 들은 ./arguments.py 나 transformer package 안의 src/transformers/training_args.py 에서 확인 가능합니다.
     # --help flag 를 실행시켜서 확인할 수 도 있습니다.
 
-    parser = HfArgumentParser(
-        (ModelArguments, DataTrainingArguments, TrainingArguments)
-    )
-    model_args, data_args, training_args = parser.parse_args_into_dataclasses()
-
+    training_args = TrainingArguments()
     training_args.do_train = True
 
-    print(f"model is from {model_args.model_name_or_path}")
-    print(f"data is from {data_args.dataset_name}")
-
-    # logging 설정
-    logging.basicConfig(
-        format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
-        datefmt="%m/%d/%Y %H:%M:%S",
-        handlers=[logging.StreamHandler(sys.stdout)],
-    )
-
-    # verbosity 설정 : Transformers logger의 정보로 사용합니다 (on main process only)
-    logger.info("Training/evaluation parameters %s", training_args)
-
-    # 모델을 초기화하기 전에 난수를 고정합니다.
-    set_seed(training_args.seed)
-
-    datasets = load_from_disk(data_args.dataset_name)
+    datasets = load_from_disk(data_args.dataset_path)
     print(datasets)
 
     # AutoConfig를 이용하여 pretrained model 과 tokenizer를 불러옵니다.
-    # argument로 원하는 모델 이름을 설정하면 옵션을 바꿀 수 있습니다.
-    config = AutoConfig.from_pretrained(
-        model_args.config_name
-        if model_args.config_name
-        else model_args.model_name_or_path,
+    model_path = os.path.join(
+        model_args.base_path, model_args.project_name, model_args.best_model_dir
     )
-    tokenizer = AutoTokenizer.from_pretrained(
-        model_args.tokenizer_name
-        if model_args.tokenizer_name
-        else model_args.model_name_or_path,
-        use_fast=True,
-    )
+
+    config = AutoConfig.from_pretrained(model_path)
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
     model = AutoModelForQuestionAnswering.from_pretrained(
-        model_args.model_name_or_path,
-        from_tf=bool(".ckpt" in model_args.model_name_or_path),
+        model_path,
         config=config,
     )
 
@@ -111,8 +70,8 @@ def run_sparse_retrieval(
     tokenize_fn: Callable[[str], List[str]],
     datasets: DatasetDict,
     training_args: TrainingArguments,
-    data_args: DataTrainingArguments,
-    data_path: str = "../../data",
+    data_args,
+    data_path: str = "../data",
     context_path: str = "wikipedia_documents.json",
 ) -> DatasetDict:
 
@@ -162,9 +121,9 @@ def run_sparse_retrieval(
 
 
 def run_mrc(
-    data_args: DataTrainingArguments,
+    data_args,
     training_args: TrainingArguments,
-    model_args: ModelArguments,
+    model_args,
     datasets: DatasetDict,
     tokenizer,
     model,
@@ -294,8 +253,6 @@ def run_mrc(
         compute_metrics=compute_metrics,
     )
 
-    logger.info("*** Evaluate ***")
-
     #### eval dataset & eval example - predictions.json 생성됨
     if training_args.do_predict:
         predictions = trainer.predict(
@@ -313,7 +270,3 @@ def run_mrc(
 
         trainer.log_metrics("test", metrics)
         trainer.save_metrics("test", metrics)
-
-
-if __name__ == "__main__":
-    main()
