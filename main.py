e8903778ed7d3ac7c1c6401a7a93a70bd106f3b1
deCyma <soos3121@gmail.com>
2021-10-25 04:36:31 +0000

    feat : argparse 추가로 config 파일을 불러옴

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import hydra
 import copy
 import os
+import argparse
 
 from omegaconf import DictConfig
 
@@ -9,21 +10,28 @@ from src.hp_opt import hp_optimizing
 from src.inference import inference
 
 
-@hydra.main(config_path=".", config_name="main_args")
-def main(cfg: DictConfig):
-    cfg = copy.deepcopy(cfg)
-    print(f"Start {cfg.project.name} !")
-    os.environ["WANDB_ENTITY"] = cfg.wandb.entity
-    os.environ["WANDB_PROJECT"] = cfg.wandb.project
-    if cfg.mode == "model_train":
-        train(cfg.project, cfg.model, cfg.data, cfg.train)
-    elif cfg.mode == "hyperparameter_tune":
-        hp_optimizing(cfg.project, cfg.model, cfg.data, cfg.hp)
-    elif cfg.mode == "retrieval_train":
-        print("dense retrieval train 만들기")
-    elif cfg.mode == "inference":
-        inference(cfg.model, cfg.data)
+def main(cf_name):
+    
+    @hydra.main(config_path="configs", config_name=cf_name)
+    def inner_main(cfg: DictConfig):
+        cfg = copy.deepcopy(cfg)
+        print(f"Start {cfg.project.name} !")
+        os.environ["WANDB_ENTITY"] = cfg.wandb.entity
+        os.environ["WANDB_PROJECT"] = cfg.wandb.project
+        if cfg.mode == "model_train":
+            train(cfg.project, cfg.model, cfg.data, cfg.train)
+        elif cfg.mode == "hyperparameter_tune":
+            hp_optimizing(cfg.project, cfg.model, cfg.data, cfg.hp)
+        elif cfg.mode == "retrieval_train":
+            print("dense retrieval train 만들기")
+        elif cfg.mode == "inference":
+            inference(cfg.model, cfg.data)
+    
+    return inner_main()
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, default='main_args', help='설정 yaml 파일을 입력해주세요.')
+    cf_name = parser.parse_args().config
+    main(cf_name)
