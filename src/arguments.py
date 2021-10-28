3be8a6d3ca9e3d68dd2a7f965ee7f232eebad4ca
deCyma <soos3121@gmail.com>
2021-10-28 02:51:13 +0000

    feat : inference 변수 추가

--- src/arguments.py
+++ src/arguments.py
@@ -1,73 +1,73 @@
 from dataclasses import dataclass, field
 
+
 @dataclass
 class ProjectArguments:
     base_path: str = field(
-        default="/opt/ml/code/models",
-        metadata={
-            "help": "프로젝트 기본 경로"
-        }
+        default="/opt/ml/code/models", metadata={"help": "프로젝트 기본 경로"}
     )
-    name: str = field(
-        default=None,
-        metadata={
-            "help": "프로젝트 이름"
-        }
-    )
-    
+    name: str = field(default=None, metadata={"help": "프로젝트 이름"})
+
+
 @dataclass
 class ModelArguments:
     """
     Arguments pertaining to which model/config/tokenizer we are going to fine-tune from.
     """
+
     name_or_path: str = field(
         default="klue/bert-base",
         metadata={
             "help": "Path to pretrained model or model identifier from huggingface.co/models"
         },
     )
-    
+
+
 @dataclass
 class DataArguments:
     dataset_path: str = field(
         default="/opt/ml/data/train_dataset",
-        metadata={
-            "help": "데이터 경로"
-        },
+        metadata={"help": "데이터 경로"},
     )
     max_length: int = field(
         default=512,
-        metadata={
-            "help": "토큰 최대길이"
-        },
+        metadata={"help": "토큰 최대길이"},
     )
     stride: int = field(
         default=128,
-        metadata={
-            "help": "토큰 겹치는 부분"
-        },
+        metadata={"help": "토큰 겹치는 부분"},
     )
     max_answer_length: int = field(
         default=32,
-        metadata={
-            "help": "정답 토큰 최대길이"
-        },
+        metadata={"help": "정답 토큰 최대길이"},
     )
+
 
 @dataclass
 class EarlyStoppingArguments:
     """
     Arguments for Early-Stopping
     """
+
     setting: bool = field(
         default=False,
-        metadata={
-            "help": "Whether to use early-stopping or not"
-        },
+        metadata={"help": "Whether to use early-stopping or not"},
     )
     patience: int = field(
         default=5,
-        metadata={
-            "help": "Patience for early-stopping"
-        },
+        metadata={"help": "Patience for early-stopping"},
+    )
+
+
+@dataclass
+class InferenceArguments:
+    topk: int = (
+        field(
+            default=5,
+            metadata={"help": "Top-K 값"},
+        ),
+    )
+    output_dir: str = field(
+        default="/opt/ml/code/outputs/sample",
+        metadata={"help": "출력장소"},
     )
