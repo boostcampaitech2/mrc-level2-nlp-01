f1beb9658087d5229027a61a7a0b85efe122ccf5
deCyma <soos3121@gmail.com>
2021-11-03 02:11:30 +0000

    feat : 1질문당 1지문으로 출력하도록 수정

--- src/retrieval.py
+++ src/retrieval.py
@@ -12,11 +12,9 @@ def make_retrieval_datasets(retrieval_cfg: RetrievalArguments):
     os.environ["WANDB_DISABLED"] = "true"
     retrieval_cfg = RetrievalArguments(**retrieval_cfg)
     # test 데이터셋 불러오기
-    result_dataset_dict = {}
     target_dataset = load_from_disk(
         os.path.join(retrieval_cfg.data_path, retrieval_cfg.target_dataset)
     )
-    dataset_key = target_dataset.keys()
 
     # top_k개 만큼 문서 검색
     retrieval_module = getattr(
@@ -27,16 +25,36 @@ def make_retrieval_datasets(retrieval_cfg: RetrievalArguments):
     retrieval = retrieval_module(**retrieval_cfg_dict)
     retrieval.get_sparse_embedding(**retrieval_cfg_dict)
 
+    if retrieval_cfg.is_join:
+        new_dataset_dict = retrieval_question_join_context(
+            target_dataset, retrieval, retrieval_cfg.k
+        )
+    else:
+        new_dataset_dict = retrieval_question_one_context(
+            target_dataset, retrieval, retrieval_cfg.k
+        )
+
+    new_dataset_dict.save_to_disk(
+        os.path.join(retrieval_cfg.data_path, retrieval_cfg.output_name)
+    )
+    print(
+        f"Success! save at {os.path.join(retrieval_cfg.data_path, retrieval_cfg.output_name)}"
+    )
+
+
+def retrieval_question_join_context(
+    target_dataset: DatasetDict, retrieval, top_k: int
+) -> DatasetDict:
+    result_dataset_dict = {}
+    dataset_key = target_dataset.keys()
     for key in dataset_key:
-        if key == "train":
-            pass  # 트레인용을 만들어라!
-        else:
+        if key == "validation":
             datas = {"scores": [], "context": [], "context_list": []}
             for data_key in target_dataset[key].column_names:
                 datas[data_key] = []
             for idx in tqdm(range(target_dataset[key].num_rows)):
                 scores, context = retrieval.get_relevant_doc(
-                    target_dataset[key]["question"][idx], k=retrieval_cfg.k
+                    target_dataset[key]["question"][idx], k=top_k
                 )
                 for data_key in target_dataset[key].column_names:
                     datas[data_key].append(target_dataset[key][data_key][idx])
@@ -44,11 +62,30 @@ def make_retrieval_datasets(retrieval_cfg: RetrievalArguments):
                 datas["context"].append(" ".join(context))
                 datas["context_list"].append(context)
             result_dataset_dict[key] = Dataset.from_dict(datas)
+    return DatasetDict(result_dataset_dict)
 
-    new_dataset_dict = DatasetDict(result_dataset_dict)
-    new_dataset_dict.save_to_disk(
-        os.path.join(retrieval_cfg.data_path, retrieval_cfg.output_name)
-    )
-    print(
-        f"Success! save at {os.path.join(retrieval_cfg.data_path, retrieval_cfg.output_name)}"
-    )
+
+def retrieval_question_one_context(target_dataset, retrieval, top_k) -> DatasetDict:
+    result_dataset_dict = {}
+    dataset_key = target_dataset.keys()
+    for key in dataset_key:
+        if key == "validation":
+            datas = {"scores": [], "context": [], "context_list": []}
+            for data_key in target_dataset[key].column_names:
+                datas[data_key] = []
+            for idx in tqdm(range(target_dataset[key].num_rows)):
+                scores, context = retrieval.get_relevant_doc(
+                    target_dataset[key]["question"][idx], k=top_k
+                )
+                for idx, ctx in enumerate(context):
+                    for data_key in target_dataset[key].column_names:
+                        if data_key == "id":
+                            datas[data_key].append(
+                                target_dataset[key][data_key][idx] + f"_{idx:03}"
+                            )
+                        else:
+                            datas[data_key].append(target_dataset[key][data_key][idx])
+                    datas["scores"].append(scores[idx])
+                    datas["context"].append(ctx)
+            result_dataset_dict[key] = Dataset.from_dict(datas)
+    return DatasetDict(result_dataset_dict)
