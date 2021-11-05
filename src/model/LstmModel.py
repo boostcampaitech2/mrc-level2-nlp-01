26be090f354c541adeb608a350ff8a3f85cef37d
decyma <soos3121@gmail.com>
2021-11-05 11:48:14 +0900

    refactor : tokenizer_name 제거

--- src/model/LstmModel.py
+++ src/model/LstmModel.py
@@ -1,18 +1,15 @@
-import torch
-import numpy as np
-from torch import nn, optim
-from torch.nn import functional as F
-from transformers import AutoTokenizer, AutoModel
+from torch import nn
+from transformers import AutoModel
 from transformers.modeling_outputs import QuestionAnsweringModelOutput
 
 
 class LstmModel(nn.Module):
-    def __init__(self, model_name, model_config, tokenizer_name):
+    def __init__(self, model_name, model_config):
         super().__init__()
         self.model_name = model_name
-        self.tokenizer_name = tokenizer_name
         self.backbone_model = AutoModel.from_pretrained(model_name, config=model_config)
-        self.lstm = nn.LSTM(input_size = model_config.hidden_size,
+        self.lstm = nn.LSTM(
+            input_size=model_config.hidden_size,
             hidden_size=model_config.hidden_size,
             num_layers=3,
             dropout=0.3,
@@ -21,43 +18,39 @@ class LstmModel(nn.Module):
         )
         self.pooler = nn.Linear(model_config.hidden_size * 2, 2)
 
-    def forward(self, input_ids=None, attention_mask=None, token_type_ids=None, 
-                position_ids=None, head_mask=None, inputs_embeds=None, 
-                start_positions=None, end_positions=None, output_attentions=None, 
-                output_hidden_states=None, return_dict=None):
-        if "xlm" in self.tokenizer_name:
-            outputs = self.backbone_model(
-                input_ids,
-                attention_mask=attention_mask,
-                token_type_ids=token_type_ids,
-                head_mask=head_mask,
-                inputs_embeds=inputs_embeds,
-                output_attentions=output_attentions,
-                output_hidden_states=output_hidden_states,
-                return_dict=return_dict,
-            )
-
-        else:
-            outputs = self.backbone_model(
-                input_ids,
-                attention_mask=attention_mask,
-                token_type_ids=token_type_ids,
-                position_ids=position_ids,
-                head_mask=head_mask,
-                inputs_embeds=inputs_embeds,
-                output_attentions=output_attentions,
-                output_hidden_states=output_hidden_states,
-                return_dict=return_dict,
-            )
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        start_positions=None,
+        end_positions=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        outputs = self.backbone_model(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
         sentence_output = outputs[0]
         outputs = self.lstm(sentence_output)
         logits = self.pooler(outputs[0])
-        
+
         start_logits, end_logits = logits.split(1, dim=-1)
         start_logits = start_logits.squeeze(-1)
         end_logits = end_logits.squeeze(-1)
-        
-        
+
         total_loss = None
         if start_positions is not None and end_positions is not None:
             if len(start_positions.size()) > 1:
@@ -77,7 +70,7 @@ class LstmModel(nn.Module):
         if not return_dict:
             output = (start_logits, end_logits) + outputs[self.pooling_pos :]
             return ((total_loss,) + output) if total_loss is not None else output
-        
+
         return QuestionAnsweringModelOutput(
             loss=total_loss,
             start_logits=start_logits,
